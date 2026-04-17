1a71552d72acf2e6996aea9bd5c09d27c70e9a92
佳伟 <s2265681@163.com>
2026-04-17 10:34:07 +0800

    fix：部署

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/China-Metting-Landing-H5/',
+  base: '/',
   plugins: [react()],
   server: {
     cors: true,
